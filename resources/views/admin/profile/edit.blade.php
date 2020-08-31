664cfb1ba4e95305dd8d895a3766c6d4b74aecfd
zakaria samy <zakariasamy10@gmail.com>
2020-08-31 22:40:52 +0200

    Added lang selector from Laravel Localization & added profile edit

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,150 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="app-content content">
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-6 col-12 mb-2">
+                <div class="row breadcrumbs-top">
+                    <div class="breadcrumb-wrapper col-12">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="">الرئيسية </a>
+                            <li class="breadcrumb-item active">الملف الشخصي
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-body">
+            <!-- Basic form layout section start -->
+            <section id="basic-form-layouts">
+                <div class="row match-height">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title" id="basic-layout-form"> تعديل  بيانات الملف الشخصي </h4>
+                                <a class="heading-elements-toggle"><i
+                                        class="la la-ellipsis-v font-medium-3"></i></a>
+                                <div class="heading-elements">
+                                    <ul class="list-inline mb-0">
+                                        <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                        <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                        <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            @include('admin.includes.alerts.success')
+                            @include('admin.includes.alerts.errors')
+                            <div class="card-content collapse show">
+                                <div class="card-body">
+                                    <form class="form" action="{{route('update.profile')}}"
+                                          method="post"
+                                          enctype="multipart/form-data">
+                                        @csrf
+                                        @method('PUT')
+
+                                         <input type="hidden" name="id" value="{{$admin -> id}}">
+
+
+                                        <div class="form-body">
+
+                                            <h4 class="form-section"><i class="ft-home"></i> تعديل بيانات الملف الشخصي </h4>
+
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="projectinput1"> الاسم </label>
+                                                        <input type="text" value="{{$admin -> name}}" id="name"
+                                                               class="form-control"
+                                                               placeholder="  "
+                                                               name="name">
+                                                        @error("name")
+                                                        <span class="text-danger">{{$message}}</span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="projectinput1"> البريد الإلكتروني </label>
+                                                        <input type="text" value="{{$admin -> email}}" id="email"
+                                                               class="form-control"
+                                                               placeholder="  "
+                                                               name="email">
+                                                        @error("email")
+                                                        <span class="text-danger">{{$message}}</span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+
+
+
+                                            </div>
+
+
+
+
+
+
+                                        </div>
+
+                                        <div class="form-body">
+
+                                            <h4 class="form-section"><i class="ft-home"></i> تعديل كلمة السر </h4>
+
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="projectinput1">كلمة السر الجديدة</label>
+                                                        <input type="password" value="" id="name"
+                                                               class="form-control"
+                                                               placeholder="  "
+                                                               name="password">
+                                                        @error("password")
+                                                        <span class="text-danger">{{$message}}</span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="projectinput1">تأكيد كلمة السر</label>
+                                                            <input type="password" value="" id="name"
+                                                                   class="form-control"
+                                                                   placeholder="  "
+                                                                   name="password_confirmation">
+                                                            @error("password_confirmation")
+                                                            <span class="text-danger">{{$message}}</span>
+                                                            @enderror
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+
+                                        <div class="form-actions">
+                                            <button type="button" class="btn btn-warning mr-1"
+                                                    onclick="history.back();">
+                                                <i class="ft-x"></i> تراجع
+                                            </button>
+                                            <button type="submit" class="btn btn-primary">
+                                                <i class="la la-check-square-o"></i> حفظ
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- // Basic form layout section end -->
+        </div>
+    </div>
+</div>
+@endsection
